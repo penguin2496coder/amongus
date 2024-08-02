c238a4d34031f8e58b9014aa5d83af5747654eff
William Li <penguin2496coder@penguincoding.org>
2024-08-02 13:49:22 -0400

    Add files via upload

---
+++ s.py
@@ -0,0 +1,20 @@
+import random
+print ("We're no strangers to love You know the rules and so do I A full commitment's what I'm thinking of You wouldn't get this from any other guy I just wanna tell you how I'm feeling Gotta make you understand Never gonna give you up Never gonna let you down Never gonna run around and desert you Never gonna make you cry Never gonna say goodbye Never gonna tell a lie and hurt you We've known each other for so long Your heart's been aching, but you're too shy to say it Inside, we both know what's been going on We know the game and we're gonna play it And if you ask me how I'm feeling Don't tell me you're too blind to see Never gonna give you up Never gonna let you down Never gonna run around and desert you Never gonna make you cry Never gonna say goodbye Never gonna tell a lie and hurt you Never gonna give you up Never gonna let you down Never gonna run around and desert you Never gonna make you cry Never gonna say goodbye Never gonna tell a lie and hurt you We've known each other for so long Your heart's been aching, but you're too shy to say it Inside, we both know what's been going on We know the game and we're gonna play it I just wanna tell you how I'm feeling Gotta make you understand Never gonna give you  Never gonna let you down Never gonna run around and desert you Never gonna make you cry Never gonna say goodbye Never gonna tell a lie and hurt you Never gonna give you up Never gonna let you down Never gonna run around and desert you Never gonna make you cry Never gonna say goodbye Never gonna tell a lie and hurt you Never gonna give you up Never gonna let you down Never gonna run around and desert you Never gonna make you cry Never gonna say goodbye Never gonna tell a lie and hurt you")
+
+
+number=random.randint(1,100000)
+score=1
+guess=int(input("Guess the number 1-100000"))
+
+               
+while guess!=number:
+    print("Incorrect")
+score=score+1
+    if guess>number:
+        print (" you guessed too high")
+    elif guess<number:
+        print("you guessed too low")
+    guess=int(input ("guess again"))
+
+print("ur finally done it took you",score,"many time")
+
